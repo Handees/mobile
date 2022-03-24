5495cfb6dd549413f669c1d01f30019377dd47c8
Omasyo <cesare3000@outlook.com>
2022-03-24 22:47:15 +0100

    more map improvements

--- lib/places_service.dart
+++ lib/places_service.dart
@@ -1,58 +1,91 @@
-import 'package:http/http.dart';
 import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:geolocator/geolocator.dart';
 
 const kMapsApiKey = 'AIzaSyA3NoblRziyftfiaqUmF5X1BYURfknrzV0';
 
-class Suggestion {
-  final String placeId;
-  final String description;
-
-  Suggestion(this.placeId, this.description);
-
-  @override
-  String toString() {
-    return 'Suggestion(description: $description, placeId: $placeId)';
-  }
-}
-
-class PlaceApiProvider {
-  final client = Client();
-
-  PlaceApiProvider(this.sessionToken);
-
-  final sessionToken;
-
-  final apiKey = kMapsApiKey;
-
-  Future<List<String>> fetchSuggestions(String input) async {
-
+class PlaceService {
+  Future<List<String>> getPredictions(String input) async {
     final request = Uri.https(
       'maps.googleapis.com',
       '/maps/api/place/autocomplete/json',
       {
-        'input' : input,//"$input",
-        'types' : 'address',
+        'input': input, //"$input",
+        'types': 'geocode', //TODO: try address
         //'language' : lang,
         //'components': 'country:ch',
-        'key':apiKey,
-        'sessionToken': sessionToken,
+        'key': kMapsApiKey,
+        //'sessionToken': sessionToken, //TODO: put token
       },
     );
-    final response = await client.get(request);
 
-    if (response.statusCode == 200) {
-      final result = json.decode(response.body);
-      if (result['status'] == 'OK') {
-        // compose suggestions in a list
-        return result['predictions']
-            .map<String>((p) => p['description']);
-      }
-      if (result['status'] == 'ZERO_RESULTS') {
-        return [];
-      }
-      throw Exception(result['error_message']);
-    } else {
-      throw Exception('Failed to fetch suggestion');
+    final response = await get(request);
+
+    final json = jsonDecode(response.body);
+
+    final result = (json['predictions'] as List)
+        .map<String>((e) => e['description'])
+        .toList();
+
+    //print(json);
+    print(result);
+
+    return result;
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
     }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
+
+//For getting suggestions
+// final request = Uri.https(
+//       'maps.googleapis.com',
+//       '/maps/api/place/autocomplete/json',
+//       {
+//         'input' : input,//"$input",
+//         'types' : 'address',
+//         //'language' : lang,
+//         //'components': 'country:ch',
+//         'key':apiKey,
+//         'sessionToken': sessionToken,
+//       },
+//     );
--- lib/screens/pick_location.dart
+++ lib/screens/pick_location.dart
@@ -1,19 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import 'package:handee/handee_colors.dart';
+import 'package:handee/places_service.dart';
 import 'package:handee/widgets/button.dart';
 
 class PickLocation extends StatefulWidget {
   const PickLocation({Key? key}) : super(key: key);
-
-  final recentAdresses = const [
-    '1 Church Road, London, E3 5GP',
-    // '27 Church Street, Wolverhampton, WV7 9KO',
-    // '94 Queens Road, Enfield, London, EN10 8KT',
-    // '12 The Crescent, Hereford, HR56 0IO''1 Church Road, London, E3 5GP',
-  ];
-
   @override
   State<PickLocation> createState() => _PickLocationState();
 }
@@ -21,6 +15,16 @@ class PickLocation extends StatefulWidget {
 class _PickLocationState extends State<PickLocation> {
   final _searchFocus = FocusNode();
   Size? _size;
+
+  final _recentAdresses = const [
+    '1 Church Road, London, E3 5GP',
+    '27 Church Street, Wolverhampton, WV7 9KO',
+    '94 Queens Road, Enfield, London, EN10 8KT',
+    '12 The Crescent, Hereford, HR56 0IO' '1 Church Road, London, E3 5GP',
+  ];
+
+  Future<List<String>>? _suggestionsFuture;
+  final _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -36,7 +40,12 @@ class _PickLocationState extends State<PickLocation> {
   @override
   void dispose() {
     _searchFocus.dispose();
+    _searchController.dispose();
     super.dispose();
+  }
+
+  Future<void> _getSuggestions(String value) async {
+    _suggestionsFuture = PlaceService().getPredictions(value);
   }
 
   @override
@@ -48,151 +57,182 @@ class _PickLocationState extends State<PickLocation> {
         body: Stack(
           children: [
             MapWidget(),
-            IgnorePointer(
-              ignoring: !_searchFocus.hasFocus,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.decelerate,
-                opacity: _searchFocus.hasFocus ? 1.0 : 0.0,
-                child: Container(
-                  color: HandeeColors.white,
-                  height: double.infinity,
-                  width: double.infinity,
-                ),
-              ),
+            FloatingActionButton(
+              onPressed: () async {
+                var res = await PlaceService().determinePosition();
+                print(res);
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 45,
-                            height: 40,
-                            child: IconButton(
-                              icon: const Icon(
-                                Icons.arrow_back,
-                                color: HandeeColors.white,
-                              ),
-                              onPressed: () => Navigator.of(context).pop(),
-                            ),
-                            decoration: BoxDecoration(
-                              color: HandeeColors.black,
-                              boxShadow: const [
-                                BoxShadow(
-                                    color: HandeeColors.shadowBlack,
-                                    blurRadius: 4),
-                              ],
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          const SizedBox(width: 13),
-                          Expanded(
-                            child: Container(
-                              //alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: HandeeColors.white,
-                                boxShadow: const [
-                                  BoxShadow(
-                                      color: HandeeColors.shadowBlack,
-                                      blurRadius: 4),
-                                ],
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              height: 40,
-                              child: TextField(
-                                onSubmitted: (_) {
-                                  setState(() {});
-                                },
-                                autofocus: false,
-                                focusNode: _searchFocus,
-                                decoration: const InputDecoration(
-                                  contentPadding: EdgeInsets.all(8),
-                                  prefixIcon: Icon(
-                                    Icons.search,
-                                    color: HandeeColors.grey89,
-                                    size: 20,
-                                  ),
-                                  border: InputBorder.none,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      AnimatedOpacity(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.decelerate,
-                        opacity: _searchFocus.hasFocus ? 1.0 : 0.0,
-                        child: Container(
-                          margin: const EdgeInsets.only(bottom: 10),
-                          alignment: Alignment.center,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: HandeeColors.lightBlue,
-                          ),
-                          child: ConstrainedBox(
-                            constraints: BoxConstraints(
-                                minHeight: 100, maxHeight: _size!.height - 220),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: FutureBuilder(
-                                future: null,
-                                builder: (context, snapshot) => false
-                                    ? ListView.builder(
-                                        shrinkWrap: true,
-                                        itemCount: widget.recentAdresses.length,
-                                        itemBuilder: (ctx, i) {
-                                          return Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Row(
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                const Icon(
-                                                  Icons.location_on_outlined,
-                                                  color: HandeeColors.blue,
-                                                  size: 18,
-                                                ),
-                                                const SizedBox(width: 12),
-                                                Expanded(
-                                                  child: Text(
-                                                    widget.recentAdresses[i],
-                                                    style: const TextStyle(
-                                                        color:
-                                                            HandeeColors.blue),
-                                                  ),
-                                                ),
-                                                const SizedBox(width: 20),
-                                              ],
-                                            ),
-                                          );
-                                        },
-                                      )
-                                    : const Padding(
-                                        padding: EdgeInsets.all(30.0),
-                                        child: CircularProgressIndicator(),
-                                      ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  HandeeButton(text: 'Done', onTap: () {}),
-                ],
-              ),
-            ),
+            // IgnorePointer(
+            //   ignoring: !_searchFocus.hasFocus,
+            //   child: AnimatedOpacity(
+            //     duration: const Duration(milliseconds: 300),
+            //     curve: Curves.decelerate,
+            //     opacity: _searchFocus.hasFocus ? 1.0 : 0.0,
+            //     child: Container(
+            //       color: HandeeColors.white,
+            //       height: double.infinity,
+            //       width: double.infinity,
+            //     ),
+            //   ),
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Column(
+            //         children: [
+            //           Row(
+            //             mainAxisAlignment: MainAxisAlignment.center,
+            //             children: [
+            //               Container(
+            //                 width: 45,
+            //                 height: 40,
+            //                 child: IconButton(
+            //                   icon: const Icon(
+            //                     Icons.arrow_back,
+            //                     color: HandeeColors.white,
+            //                   ),
+            //                   onPressed: () => Navigator.of(context).pop(),
+            //                 ),
+            //                 decoration: BoxDecoration(
+            //                   color: HandeeColors.black,
+            //                   boxShadow: const [
+            //                     BoxShadow(
+            //                         color: HandeeColors.shadowBlack,
+            //                         blurRadius: 4),
+            //                   ],
+            //                   borderRadius: BorderRadius.circular(5),
+            //                 ),
+            //               ),
+            //               const SizedBox(width: 13),
+            //               Expanded(
+            //                 child: Container(
+            //                   //alignment: Alignment.center,
+            //                   decoration: BoxDecoration(
+            //                     color: HandeeColors.white,
+            //                     boxShadow: const [
+            //                       BoxShadow(
+            //                           color: HandeeColors.shadowBlack,
+            //                           blurRadius: 4),
+            //                     ],
+            //                     borderRadius: BorderRadius.circular(5),
+            //                   ),
+            //                   height: 40,
+            //                   child: TextField(
+            //                     controller: _searchController,
+            //                     onSubmitted: (value) {
+            //                       //setState(() {});
+            //                       _getSuggestions(value);
+            //                     },
+            //                     onChanged: (value) {
+            //                       setState(() {
+            //                         _getSuggestions(value);
+            //                       });
+            //                     },
+            //                     autofocus: false,
+            //                     focusNode: _searchFocus,
+            //                     decoration: const InputDecoration(
+            //                       contentPadding: EdgeInsets.all(8),
+            //                       prefixIcon: Icon(
+            //                         Icons.search,
+            //                         color: HandeeColors.grey89,
+            //                         size: 20,
+            //                       ),
+            //                       border: InputBorder.none,
+            //                     ),
+            //                   ),
+            //                 ),
+            //               ),
+            //             ],
+            //           ),
+            //           const SizedBox(height: 10),
+            //           AnimatedOpacity(
+            //             duration: const Duration(milliseconds: 300),
+            //             curve: Curves.decelerate,
+            //             opacity: _searchFocus.hasFocus ? 1.0 : 0.0,
+            //             child: Container(
+            //               margin: const EdgeInsets.only(bottom: 10),
+            //               alignment: Alignment.center,
+            //               width: double.infinity,
+            //               decoration: BoxDecoration(
+            //                 borderRadius: BorderRadius.circular(8),
+            //                 color: HandeeColors.lightBlue,
+            //               ),
+            //               child: ConstrainedBox(
+            //                 constraints: BoxConstraints(
+            //                     minHeight: 100, maxHeight: _size!.height - 220),
+            //                 child: Padding(
+            //                   padding: const EdgeInsets.all(8.0),
+            //                   child: _searchController.text.isNotEmpty
+            //                       ? FutureBuilder<List<String>>(
+            //                           future: _suggestionsFuture,
+            //                           builder: (context, snapshot) {
+            //                             return snapshot.connectionState ==
+            //                                     ConnectionState.done
+            //                                 ? SuggestionsWidget(snapshot.data!)
+            //                                 : const Padding(
+            //                                     padding: EdgeInsets.all(30.0),
+            //                                     child:
+            //                                         CircularProgressIndicator(),
+            //                                   );
+            //                           },
+            //                         )
+            //                       : SuggestionsWidget(_recentAdresses),
+            //                 ),
+            //               ),
+            //             ),
+            //           ),
+            //         ],
+            //       ),
+            //       HandeeButton(text: 'Done', onTap: () {}),
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class SuggestionsWidget extends StatelessWidget {
+  const SuggestionsWidget(
+    this.suggestions, {
+    Key? key,
+  }) : super(key: key);
+
+  final List<String> suggestions;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: suggestions.length, //_recentAdresses.length,
+      itemBuilder: (ctx, i) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.location_on_outlined,
+                color: HandeeColors.blue,
+                size: 18,
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Text(
+                  suggestions[i],
+                  style: const TextStyle(color: HandeeColors.blue),
+                ),
+              ),
+              const SizedBox(width: 20),
+            ],
+          ),
+        );
+      },
     );
   }
 }
@@ -209,18 +249,16 @@ class MapWidget extends StatefulWidget {
 class _MapWidgetState extends State<MapWidget> {
   @override
   Widget build(BuildContext context) {
-    return const GoogleMap(
+    return GoogleMap(
       initialCameraPosition: CameraPosition(
         target: LatLng(6.518, 3.378),
         zoom: 15,
       ),
-      compassEnabled: false,
-      buildingsEnabled: false,
-      //liteModeEnabled: true,
-      // scrollGesturesEnabled: true,
-      // tiltGesturesEnabled: true,
-      // rotateGesturesEnabled: true,
-      // zoomGesturesEnabled: true,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+      mapToolbarEnabled: true,
+
+      //zoomControlsEnabled: false,
     );
   }
 }
