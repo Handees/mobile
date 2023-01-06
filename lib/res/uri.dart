94f9e7df4edc124296846f287a45be4d56fb908c
Omasyo <78266620+Omasyo@users.noreply.github.com>
2023-01-06 20:51:03 +0100

    Update uri.dart

--- lib/res/uri.dart
+++ lib/res/uri.dart
@@ -1,6 +1,21 @@
 import 'package:handees/res/constants.dart';
 
 abstract class AppUris {
+  static final customerSocketUri = Uri.http(
+    AppConstants.url,
+    '/cutomer/',
+  );
+
+  static final artisanSocketUri = Uri.http(
+    AppConstants.url,
+    '/artisan/',
+  );
+
+  static final chatSocketUri = Uri.http(
+    AppConstants.url,
+    '/chat/',
+  );
+
   static final addNewUserUri = Uri.http(
     AppConstants.url,
     '/api/user/',
