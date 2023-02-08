ffff07080580f335c333490691e25529eabdc89b
Omasyo <cesare3000@outlook.com>
2023-02-08 16:52:31 +0100

    added socket connections

---
+++ lib/data/chats/datasources/remote.dart
@@ -0,0 +1,8 @@
+import 'package:firebase/firebase.dart';
+import 'package:handees/services/sockets.dart';
+
+class ChatsRemoteDataSource {
+  final appsockets = AppSockets.instance;
+
+  void sendMessage() {}
+}
--- lib/services/sockets.dart
+++ lib/services/sockets.dart
@@ -1,21 +1,17 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:handees/res/uri.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 
 class AppSockets {
   AppSockets._() {
-    print('try connect');
-    customerSocket.connect();
-    chatSocket.connect();
-
-    customerSocket.onConnect((_) {
-      print('customer connected');
-    });
-    chatSocket.onConnect((_) {
-      print('Chat connected');
-    });
+    customerSocket.onConnect((_) => debugPrint('customer connected'));
+    chatSocket.onConnect((_) => debugPrint('Chat connected'));
     // socket.on('event', (data) => print('Event $data'));
-    customerSocket.onDisconnect((_) => print('client disconnected'));
-    chatSocket.onDisconnect((_) => print('Chat disconnected'));
+    customerSocket.onDisconnect((_) => debugPrint('client disconnected'));
+    chatSocket.onDisconnect((_) => debugPrint('Chat disconnected'));
 
     customerSocket.on('msg', (data) {
       print('Customer update: $data');
@@ -25,10 +21,52 @@ class AppSockets {
       print('Chat update: $data');
     });
   }
+  static final AppSockets _instance = AppSockets._();
+  static AppSockets get instance => _instance;
 
   io.Socket customerSocket = io.io(AppUris.customerSocketUri.toString(),
       io.OptionBuilder().setTransports(['websocket']).build());
 
   io.Socket chatSocket = io.io(AppUris.chatSocketUri.toString(),
       io.OptionBuilder().setTransports(['websocket']).build());
+
+  void disconnectAll() {
+    chatSocket.disconnect();
+    customerSocket.disconnect();
+  }
+
+  void connectChat() => chatSocket.connect();
+  void disconnectChat() => chatSocket.disconnect();
+  void emitChatEvent(String event, dynamic data) {
+    if (chatSocket.disconnected) throw const SocketException.closed();
+    chatSocket.emit(event, data);
+  }
+
+  Stream<T> onChatEvent<T>(String event) {
+    if (chatSocket.disconnected) throw const SocketException.closed();
+
+    final controller = StreamController<T>();
+
+    chatSocket.on(event, (data) {
+      print(data);
+      controller.add(data);
+    });
+
+    return controller.stream;
+  }
+
+  void connectCustomer() => customerSocket.connect();
+  void disconnectCustomer() => customerSocket.connect();
+  Stream<T> onCustomerEvent<T>(String event) {
+    if (customerSocket.disconnected) throw const SocketException.closed();
+
+    final controller = StreamController<T>();
+
+    customerSocket.on(event, (data) {
+      print(data);
+      controller.add(data);
+    });
+
+    return controller.stream;
+  }
 }
