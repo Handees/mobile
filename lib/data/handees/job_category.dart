a3a2d4981bb9bcea4cab6ea614706785e2143e56
Omasyo <cesare3000@outlook.com>
2023-02-17 16:54:11 +0100

    added launcher icon

--- lib/data/handees/job_category.dart
+++ lib/data/handees/job_category.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:handees/res/icons.dart';
 
 class JobCategory {
   final String name;
@@ -21,7 +22,7 @@ abstract class _JobCategories {
   static const laundry = JobCategory._(
     name: 'Laundry',
     id: 'laundry',
-    icon: Icons.abc,
+    icon: HandeeIcons.laundry,
     foregroundColor: Color.fromRGBO(65, 141, 244, 1),
   );
   static const carpentry = JobCategory._(
@@ -33,31 +34,31 @@ abstract class _JobCategories {
   static const housekeeping = JobCategory._(
     name: "Housekeeping",
     id: "housekeeping",
-    icon: Icons.abc,
+    icon: HandeeIcons.housekeeping,
     foregroundColor: Color.fromRGBO(255, 161, 154, 1),
   );
   static const gardening = JobCategory._(
     name: "Gardening",
     id: "gardening",
-    icon: Icons.alarm,
+    icon: HandeeIcons.gardening,
     foregroundColor: Color.fromRGBO(57, 186, 112, 1),
   );
   static const autoshop = JobCategory._(
     name: "Auto Shop",
     id: "autoshop",
-    icon: Icons.ads_click,
+    icon: HandeeIcons.autoshop,
     foregroundColor: Color.fromRGBO(80, 85, 92, 1),
   );
   static const barber = JobCategory._(
     name: "Barber",
     id: "barber",
-    icon: Icons.baby_changing_station,
+    icon: HandeeIcons.barber,
     foregroundColor: Color.fromRGBO(230, 180, 27, 1),
   );
   static const hairStylist = JobCategory._(
     name: "Hair Stylist",
     id: "hair-stylist",
-    icon: Icons.hail_rounded,
+    icon: HandeeIcons.hair_brush,
     foregroundColor: Color.fromRGBO(255, 125, 203, 1),
   );
 }
@@ -71,6 +72,9 @@ const jobCategories = [
   _JobCategories.housekeeping,
   _JobCategories.laundry,
   _JobCategories.barber,
-  _JobCategories.barber,
-  _JobCategories.barber,
+
+  //repeated
+  _JobCategories.autoshop,
+  _JobCategories.carpentry,
+  _JobCategories.gardening,
 ];
